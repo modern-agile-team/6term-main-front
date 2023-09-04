9148ccfb535bbbc32e3376610514bb9cc5cfca0a
zzzRYT <jinjinstar3@gmail.com>
2023-09-04 20:23:57 +0900

    publish(#3): before merge

---
+++ src/components/organisms/common/Header.tsx
@@ -0,0 +1,10 @@
+import NavBar from "@/components/molecules/navBar/NavBar";
+
+export default function Header() {
+    return (
+        <div>
+            <NavBar />
+            <div>헤더입니다.</div>
+        </div>
+    );
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import Footer from '@/components/organisms/common/Footer'
 import SideBar from '@/components/organisms/common/SideBar'
+import Header from '@/components/organisms/common/Header'
 import type { AppProps } from 'next/app'
 import { RecoilRoot } from 'recoil'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
+      <Header />
       <SideBar />
       <Component {...pageProps} />
       <Footer />
