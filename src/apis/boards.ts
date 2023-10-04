967e2d9102c25e42e4e43ab400bf76896fd20d12
zzzRYT <jinjinstar3@gmail.com>
2023-10-04 15:55:37 +0900

    modifiy(#29): 글 작성 페이지 완성

--- src/apis/boards.ts
+++ src/apis/boards.ts
@@ -18,19 +18,22 @@ const BOARDS = {
     main_category,
     sub_category,
   }: Post): Promise<any> {
-    const result: AxiosResponse = await instance.post<Post>(`${BOARDS.path}`, {
-      head: head,
-      body: body,
-      main_category: main_category,
-      sub_category: sub_category,
-    });
+    const result: AxiosResponse = await instance.post<Post>(
+      `${BOARDS.path}/post`,
+      {
+        head: head,
+        body: body,
+        main_category: main_category,
+        sub_category: sub_category,
+      },
+    );
     return result;
   },
 
   //이미지 업로드 api
   async createImg(image: FormData, boardId: number): Promise<any> {
     const result: AxiosResponse = await instance.post(
-      `${BOARDS.path}/images/${boardId}`,
+      `${BOARDS.path}/${boardId}/images`,
       image,
       {
         headers: {
