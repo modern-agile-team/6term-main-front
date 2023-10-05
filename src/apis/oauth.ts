346ad39f0c7a8b39dc89768ce632886dfec650a5
zzzRYT <jinjinstar3@gmail.com>
2023-10-05 13:20:28 +0900

    modifiy(#23): kakao기능 수정

--- src/apis/oauth.ts
+++ src/apis/oauth.ts
@@ -24,52 +24,45 @@ const getTokenKakao = async (code: string) => {
   }
 };
 
-// const getTokenNaver = async (code: string) => {
-//   const NAVER_URL = `https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=${process.env.NEXT_PUBLIC_NAVER_CLIENT_ID}&client_secret=${process.env.NEXT_PUBLIC_NAVER_CLIENT_SECRET}&code=${code}&state=test`;
-//   //   const NAVER_URL = `https://nid.naver.com/oauth2.0/token`;
-//   const response = await axios.post(
-//     NAVER_URL,
-//     {
-//       grant_type: 'authorization_code',
-//       client_id: process.env.NEXT_PUBLIC_NAVER_CLIENT_ID,
-//       client_secret: process.env.NEXT_PUBLIC_NAVER_CLIENT_SECRET,
-//       code: code,
-//       state: 'test',
-//     },
-//     {
-//       headers: {
-//         'Content-Type': 'application/json',
-//         'Access-Control-Allow-Origin': 'http://localhost:3000',
-//         'Access-Control-Allow-Credentials': 'true',
-//       },
-//     },
-//   );
-//   return response.data;
-// };
-
 const getTokenNaver = async (code: string) => {
-  const KAKAO_URL = 'https://nid.naver.com/oauth2.0/token';
-
-  try {
-    const response = await axios.post(
-      KAKAO_URL,
-      {
-        grant_type: 'authorization_code',
-        client_id: process.env.NEXT_PUBLIC_NAVER_CLIENT_ID,
-        client_secret: process.env.NEXT_PUBLIC_NAVER_CLIENT_SECRET,
-        code: code,
-        state: 'test',
-      },
-      {
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
-        },
-      },
-    );
-    return response.data;
-  } catch (err) {
-    return err;
-  }
+  const NAVER_URL = `https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=${process.env.NEXT_PUBLIC_NAVER_CLIENT_ID}&client_secret=${process.env.NEXT_PUBLIC_NAVER_CLIENT_SECRET}&code=${code}&state=test`;
+  //   const NAVER_URL = `https://nid.naver.com/oauth2.0/token`;
+  const response = await axios.post(NAVER_URL, {
+    mode: 'no-cors',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': 'http://localhost:3000',
+      'Access-Control-Allow-Credentials': 'true',
+    },
+    withCredentials: true,
+    credentials: 'same-origin',
+  });
+  return response.data;
 };
+
+// const getTokenNaver = async (code: string) => {
+//   const KAKAO_URL = 'https://nid.naver.com/oauth2.0/token';
+
+//   try {
+//     const response = await axios.post(
+//       KAKAO_URL,
+//       {
+//         grant_type: 'authorization_code',
+//         client_id: process.env.NEXT_PUBLIC_NAVER_CLIENT_ID,
+//         client_secret: process.env.NEXT_PUBLIC_NAVER_CLIENT_SECRET,
+//         code: code,
+//         state: 'test',
+//       },
+//       {
+//         headers: {
+//           'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+//         },
+//       },
+//     );
+//     return response.data;
+//   } catch (err) {
+//     return err;
+//   }
+// };
 
 export { getTokenKakao, getTokenNaver };
