d52be82e589350c4cf6703b8918a17efde392e9f
CBWDG <zaratole@naver.com>
2023-10-17 17:54:18 +0900

    feat(#40): 채팅room 퍼블리싱

--- src/components/molecules/chatting/direct-message/dm.tsx
+++ src/components/molecules/chatting/direct-message/dm.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import ChatSpace from '../chat-modal/ChatSpace';
 
 const DirectMessage = () => {
-  return (
-    <ChatSpace>
-      <div>CrabChat에 오신 것을 환영합니다!!</div>
-    </ChatSpace>
-  );
+  return 'hi';
 };
 
 export default DirectMessage;
