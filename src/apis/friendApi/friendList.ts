b8c28a75d211d1d8c50302a7b12d679dd4b863ee
CBWDG <zaratole@naver.com>
2023-11-02 21:11:25 +0900

    feat(#64): 친구 요청 Api 추가

--- src/apis/friendApi/friendList.ts
+++ src/apis/friendApi/friendList.ts
@@ -18,6 +18,30 @@ const FRIENDS = {
     return result;
   },
 
+  // 내가 요청 보낸 친구 목록 api(get)
+  async requestedList(): Promise<any> {
+    const result: AxiosResponse = await instance.get<Friend>(
+      `${FRIENDS.path}/requests/pending`,
+    );
+    return result;
+  },
+
+  // 내가 요청 받은 친구 목록 api(get)
+  async responsedList(): Promise<any> {
+    const result: AxiosResponse = await instance.get<Friend>(
+      `${FRIENDS.path}/responses/pending`,
+    );
+    return result;
+  },
+
+  // 차단 목록(영구 거절) api(get)
+  async rejectList(): Promise<any> {
+    const result: AxiosResponse = await instance.get<Friend>(
+      `${FRIENDS.path}/responses/reject/permanent`,
+    );
+    return result;
+  },
+
   // 친구 요청 api(post)
   async friendRequest(): Promise<any> {
     const result: AxiosResponse = await instance.post<Friend>(
@@ -26,17 +50,5 @@ const FRIENDS = {
     return result;
   },
 
-  // 내가 요청 보낸 친구 목록 api(get)
-  async requestedList(): Promise<any> {
-    const result: AxiosResponse = await instance.get<Friend>(`${FRIENDS.path}/requests/pending`)
-    return result;
-  },
-
-  // 내가 요청 받은 친구 목록 api(get)
-  async responseList(): Promise<any> {
-    const result: AxiosResponse = await instance.get<Friend>(`${FRIENDS.path}/responses/pending`)
-    return result;
-  },
-
-  // 차단 목록
+  // 친구 요청 수락 api(post)
 };
