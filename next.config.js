f6010b3c35e615a2f38b4e3df7d06bb3f03822b4
CBWDG <zaratole@naver.com>
2023-11-14 11:28:19 +0900

    fix(#77): vercel 긴급 수정

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-
+  compiler: {
+    styledComponents: true,
+  },
   images: {
     domains: ['ma6-main.s3.ap-northeast-2.amazonaws.com'],
   },
