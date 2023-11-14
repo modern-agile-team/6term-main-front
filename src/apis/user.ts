11fad580628c16eafeba3664b0671eb49ffb535e
CBWDG <zaratole@naver.com>
2023-11-14 15:13:55 +0900

    feat(#83): 미로그인 시 403에러 핸들링 시도

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -16,8 +16,17 @@ const USERS = {
 
   //유저정보조회api
   async getUserProfile(): Promise<UserInfo> {
-    const result: AxiosResponse = await instance.get(`${USERS.path}/my-info`);
-    return result.data;
+    try {
+      const result: AxiosResponse = await instance.get(`${USERS.path}/my-info`);
+      return result.data;
+    } catch (error: any) {
+      if (error.response && error.response.status === 403) {
+        // 403 에러 발생 시 로그인하지 않은 상태로 간주하고 빈 객체 반환
+        return {} as UserInfo;
+      }
+      // 다른 에러는 그대로 throw
+      throw error;
+    }
   },
 };
 
